2f9f6d8955e918dc1c89a0a1fe50b94179defb85
RyoNozu <rnozu22@hiroshima-u.ac.jp>
2025-01-13 11:47:11 -0800

    WIP

--- 06_Generate_BSgenome_Package_4_target_species.r
+++ 06_Generate_BSgenome_Package_4_target_species.r
@@ -1,5 +1,7 @@
 # generate BSgenome Package for target species
-BSgenomeForge::forgeBSgenomeDataPkg("./BSgenome.Htrimaculatus.Htriv1.1-seed.txt", replace=TRUE)
-devtools::build("./BSgenome.Htrimaculatus.inhouse.Htriv1")
+# in this case, H. trimaculatus
+# seed file: BSgenome.Htrimaculatus.Htriv1.1-seed.txt
+BSgenomeForge::forgeBSgenomeDataPkg(".+-seed.txt", replace=TRUE)
+devtools::build("BSgenome.Htrimaculatus.inhouse.Htriv1")
 devtools::check_built("BSgenome.Htrimaculatus.inhouse.Htriv1_1.0.0.tar.gz")
 devtools::install_local("BSgenome.Htrimaculatus.inhouse.Htriv1_1.0.0.tar.gz")
