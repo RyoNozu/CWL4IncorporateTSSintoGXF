d0d4981b5e99a0bec4d2751ab77e91f238982ee1
RyoNozu <rnozu22@hiroshima-u.ac.jp>
2025-02-17 13:51:23 +0900

    removed R script step05, 06, 07, 08

--- 06_Generate_BSgenome_Package_4_target_species.r
+++
@@ -1,7 +0,0 @@
-# generate BSgenome Package for target species
-# in this case, H. trimaculatus
-# seed file: BSgenome.Htrimaculatus.Htriv1.1-seed.txt
-BSgenomeForge::forgeBSgenomeDataPkg(".+-seed.txt", replace=TRUE)
-devtools::build("BSgenome.Htrimaculatus.inhouse.Htriv1")
-devtools::check_built("BSgenome.Htrimaculatus.inhouse.Htriv1_1.0.0.tar.gz")
-devtools::install_local("BSgenome.Htrimaculatus.inhouse.Htriv1_1.0.0.tar.gz")
